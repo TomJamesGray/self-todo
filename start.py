bbc5cb5491fb6758e3ce932cbcbe2fe79f182300
TomJamesGray <tomg77@yahoo.co.uk>
2016-06-28 10:55:57 +0100

    Basic files

---
+++ start.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+#TODO check that config exists and contains needed paramaters, and dependencies
+from src import main
+def start():
+    main.main()
+
+if __name__ == "__main__":
+    start()

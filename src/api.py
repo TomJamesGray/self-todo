a048aa8eade8ae5fc92fbf29e9f7d74b7a670c72
TomJamesGray <tomg77@yahoo.co.uk>
2016-06-28 14:20:41 +0100

    Verifying that list name provided is unique

--- src/api.py
+++ src/api.py
@@ -2,6 +2,20 @@ import oursql
 class Api(object):
     def __init__(self,conn):
         self.conn = conn
-        self.cursor = self.conn.cursor(oursql.DictCursor)
+        self.cursor = self.conn.cursor()
+    
     def createList(self,listName):
         self.cursor.execute("INSERT INTO todoLists SET listName=?",(listName,))
+    
+    #Retrieve all columns for lists from db
+    #Columns should be comma seperated
+    def getLists(self,columns):
+        columnNames = ['listId','listName','creationDate']
+        columnsList = columns.split(",")
+
+        for column in columnsList:
+            if column not in columnNames:
+                raise ValueError("Column name desired provided is not in column names")
+
+        self.cursor.execute("SELECT {}  FROM todoLists".format(columns))
+        return self.cursor.fetchall()
--- src/client.py
+++ src/client.py
@@ -6,6 +6,16 @@ class Client():
 
     def createListPrompt(self):
         listName = input("create > ")
+        #Check if that list name is unique
+        try:
+            namesOfExistingLists = self.api.getLists("listName")
+        except ValueError as e:
+            print(e)
+            return False
+        if (listName,) in namesOfExistingLists:
+            print("List name already exists")
+            return False
+
         self.api.createList(listName)    
 
     def addListItem(self):

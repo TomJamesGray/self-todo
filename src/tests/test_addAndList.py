111db502b1b54a0c2fb4b2b629a86b1eb226e4bc
TomJamesGray <tomg77@yahoo.co.uk>
2016-06-30 12:37:02 +0100

    removed test file that was accidentally commited and was just used for temporary testing

--- src/tests/test_addAndList.py
+++
@@ -1,7 +0,0 @@
-from src.api import Api
-def test_addAndList():
-    api = Api("192.168.1.2","example","password","self_todo")
-    api.getListItems(4,"content,completed")
-    api.saveListItem(4,"test",0)
-    print(api.getListItems(4,"content,completed"))
-    assert False

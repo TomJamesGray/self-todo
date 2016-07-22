c5f5ec6af2849196f1b6a81c95dba5092a977be7
TomJamesGray <tomg77@yahoo.co.uk>
2016-07-22 15:34:07 +0100

    Added base template and template for index

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask
+from flask import Flask,render_template
 from src.api import Api
 
 app = Flask(__name__)
@@ -7,7 +7,12 @@ api = Api('192.168.1.2','example','password','self_todo')
 
 @app.route('/')
 def index():
-    return str(api.getLists(['listName']))
+    #Get lists and get the list names out of the tuple
+    lists = api.getLists()
+    a = []
+    for i in lists:
+        a.append(i[0])
+    return render_template('index.html',lists=a)
 
 @app.route('/list/<listName>')
 def show_todos(listName):

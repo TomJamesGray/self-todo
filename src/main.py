422b5429bfaf515395d33839c96faaf5b2c6d544
TomJamesGray <tomg77@yahoo.co.uk>
2016-07-22 14:42:34 +0100

    Getting lists in /

--- src/main.py
+++ src/main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-from configparser import SafeConfigParser
 from flask import Flask
-import os
-import oursql
-import sys
+from src.api import Api
 
 app = Flask(__name__)
+api = Api('192.168.1.2','example','password','self_todo')
 
 @app.route('/')
 def index():
-    return "Show all todo lists"
+    return str(api.getLists(['listName']))
 
 @app.route('/list/<listName>')
 def show_todos(listName):

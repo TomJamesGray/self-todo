7eab7175fefe8966bd64afa4e102346c016e3c3b
TomJamesGray <tomg77@yahoo.co.uk>
2016-07-28 16:12:03 +0100

    Don't know why this wasn't in the last commit

--- helpers.py
+++
@@ -1,8 +0,0 @@
-import os
-from configparser import SafeConfigParser
-
-def getConfPart(section,key):
-    parser = SafeConfigParser()
-    location = os.path.abspath("config.ini")
-    parser.read(location)
-    return parser.get(section,key)

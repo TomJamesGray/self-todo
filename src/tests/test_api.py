33a698821465587e2aa8b8cdb6518e8b32f4ce8b
TomJamesGray <tomg77@yahoo.co.uk>
2016-08-01 16:38:11 +0100

    Fixed tests failing by adding userId in on required functions

--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -4,25 +4,30 @@ import pytest
 listName = "pyTestList"
 listId = None
 todoId = None
+userId = None
 todoContents = "todo content"
 @pytest.fixture(scope="module")
 def makeList(request):
-    global listName,listId,todoContents
+    global listName,listId,todoContents,userId
     client = Client()
-    client.api.createList("pyTestList")
+    client.api.createUser("travis","password")
+    userId = client.api.getUserId("travis")
+    client.api.createList("pyTestList",userId)
     listId = client.api.getListId("pyTestList")
     client.api.saveListItem(listId,todoContents,0)
     def removeList():
         #Remove todo first to stop foreign key error
         client.api.removeListItem(todoId[0][0])
         client.api.removeList(listId)
+        client.api.deleteUser(userId)
         client.api.closeConnection()
     request.addfinalizer(removeList)
     return client.api
 
 def testListIsPresent(makeList):
-    global listName
-    if ("pyTestList",) in makeList.getLists(["listName"]):
+    global listName,userId
+    print(userId)
+    if ("pyTestList",) in makeList.getLists(userId,["listName"]):
         assert True
     else:
         assert False
